ec767bdde58331f9119f1dc1839d56cf608b3261
François Caillet (alias Funflash) <Francois.caillet@gmail.com>
2016-02-03 14:24:44 +0100

    init classes

--- src/model/Worthless.java
+++ src/model/Worthless.java
@@ -1,13 +1,8 @@
 package model;
 
-import java.util.Vector;
-
 /**
  * Created by François Caillet on 03/02/2016.
  * All rights reserved.
  */
-public class Environnement {
-
-    private Vector<Content> contents;
-
+public class Worthless {
 }
